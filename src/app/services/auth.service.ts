c632e3a8eb9690abe10d1a4e5342e2ffc3c1b287
Andrea <turazzandrea@gmail.com>
2024-08-10 15:09:10 +0200

    Completed

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,15 +4,39 @@ import {
   AngularFirestore,
   AngularFirestoreCollection,
 } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 import IUser from '../models/user.model';
+import { map, delay, filter, switchMap, of } from 'rxjs';
+import { Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private userCollection: AngularFirestoreCollection<IUser>;
-  constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
+  public isAuthenticated$: Observable<boolean>;
+  public isAuthenticatedWithDelay$: Observable<boolean>;
+  public redirect = false;
+
+  constructor(
+    private auth: AngularFireAuth,
+    private db: AngularFirestore,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
     this.userCollection = db.collection('users');
+    this.isAuthenticated$ = auth.user.pipe(map((user) => !!user));
+    this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(delay(1000));
+    this.router.events
+      .pipe(
+        filter((e) => e instanceof NavigationEnd),
+        map((e) => this.route.firstChild),
+        switchMap((route) => route?.data ?? of({ authOnly: false }))
+      )
+      .subscribe((data) => {
+        this.redirect = data.authOnly ?? false;
+      });
   }
 
   public async createUser(userData: IUser) {
@@ -23,6 +47,7 @@ export class AuthService {
       userData.email as string,
       userData.password as string
     );
+
     if (!userCred.user) {
       throw new Error("User can't be found");
     }
@@ -36,5 +61,16 @@ export class AuthService {
     await userCred.user.updateProfile({
       displayName: userData.name,
     });
+  }
+
+  public async logout($event?: Event) {
+    if ($event) {
+      $event.preventDefault();
+    }
+    await this.auth.signOut();
+
+    if (this.redirect) {
+      await this.router.navigateByUrl('/');
+    }
   }
 }
